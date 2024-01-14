916d913c13b50420c91c14779cbea5d25ef28459
Makoma Cathrine Sekhuto <135700783+CathrineZA@users.noreply.github.com>
2024-01-14 19:35:57 +0200

    added a link to Github

--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,5 @@
 </div>
  <footer class="section">
-   <div class="center" grey-text>Coded by Cathy Sekhuto and its open sauced on <a href="#">Github</a></div>
+   <div class="center" grey-text>Coded by Cathy Sekhuto and its open source on <a href="https://github.com/CathrineZA/the-blog.git">Github</a></div>
  </footer>
 </body>
